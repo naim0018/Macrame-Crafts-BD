62da00cc313bdd477d95454ecd42d89df32fa6d9
naim0018 <mdkazinaim0018@gmail.com>
2023-11-27 00:29:04 +0600

    Add Loading and Error page

---
+++ src/Component/Pages/Loading/Loading.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Lottie from 'react-lottie';
+import loading from './loading.json'
+const Loading = () => {
+
+
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: loading,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
+        }
+        
+    };
+
+    return (
+        <div>
+            <Lottie options={defaultOptions}
+                height={400}
+                width={400}
+                isClickToPauseDisabled={true}
+            />
+        </div>
+    );
+};
+
+export default Loading;
