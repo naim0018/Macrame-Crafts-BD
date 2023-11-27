f2ae89f92f2b05895ef19425e6f24e43d8899c11
naim0018 <rakibhasan1101@gmail.com>
2023-11-27 13:23:14 +0600

    naim

--- src/Component/MainLayout/MainLayout.jsx
+++ src/Component/MainLayout/MainLayout.jsx
@@ -14,9 +14,9 @@ const MainLayout = ({ children }) => {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
               </label>
             </div>
-            <div className="flex-1 px-2 mx-2"><img className="border rounded-full w-20" src="https://i.ibb.co/YpDz3JN/macrime-logo.png" alt="" /></div>
+            <div className="flex-1 px-2 mx-2"><img className="border rounded-full w-16" src="https://i.ibb.co/YpDz3JN/macrime-logo.png" alt="" /></div>
             <div className="flex-none hidden lg:block">
-              <ul className="menu menu-horizontal">
+              <ul className="border">
                 {/* Navbar menu content here */}
                 <Navbar />
               </ul>
@@ -27,7 +27,7 @@ const MainLayout = ({ children }) => {
         </div>
         <div className="drawer-side">
           <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
-          <ul className="menu p-4 w-80 min-h-full bg-base-200">
+          <ul className=" p-4 w-80 min-h-full bg-base-200">
             {/* Sidebar content here */}
             <Navbar />
           </ul>
