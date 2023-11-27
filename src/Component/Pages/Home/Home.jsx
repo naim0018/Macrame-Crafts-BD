ac9dcd568a0f61e4425e275f39e3f887f67dea38
azizulislam74 <azizulislam74@gmail.com>
2023-11-27 11:07:17 +0600

    started

--- src/Component/Pages/Home/Home.jsx
+++ src/Component/Pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import Loading from '../Loading/Loading';
 
 const Home = () => {
     return (
         <div>
             <h1>inside Home</h1>
+            <Loading></Loading>
         </div>
     );
 };
