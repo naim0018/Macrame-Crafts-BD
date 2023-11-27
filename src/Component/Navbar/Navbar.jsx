99389bf3218dfe58aea6e1963ac08aacb35a67a3
naim0018 <rakibhasan1101@gmail.com>
2023-11-27 16:08:17 +0600

    add

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
    <>
    <div className="flex flex-col lg:flex-row items-center justify-center font-semibold gap-5 ">
      <NavLink to='/'
-     className={({isActive})=> isActive ? "p-4 text-red-500" : "p-4 hover:text-white"}>Home</NavLink>
+     className={({isActive})=> isActive ? "p-4 text-red-500" : "p-4 hover:text-red-500"}>Home</NavLink>
      <NavLink to='/products'
      className={({isActive})=> isActive ? "p-4 text-red-500" : "p-4 hover:text-red-500"}>Products</NavLink>
      <NavLink to='/tools'
