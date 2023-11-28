3ed57aa136583816da1e52ff712f0a432f8bd1d2
azizulislam74 <azizulislam74@gmail.com>
2023-11-28 11:44:02 +0600

    slider add done

--- src/Component/Pages/Banner/Slider/Slider.jsx
+++ src/Component/Pages/Banner/Slider/Slider.jsx
@@ -34,15 +34,10 @@ const Slider = () => {
         
         className="mySwiper"
       >
-        <SwiperSlide><img className="w-96 h-96" src="https://i.ibb.co/cXH8QTr/product1.jpg" alt="" /></SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide><img  src="https://i.ibb.co/nLSWr2m/banner-2.png" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="https://i.ibb.co/nLSWr2m/banner-2.png" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="https://i.ibb.co/nLSWr2m/banner-2.png" alt="" /></SwiperSlide>
+     
    
       </Swiper>
     </div>
