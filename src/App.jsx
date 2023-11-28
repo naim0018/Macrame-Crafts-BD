c0510b93ec958ef1cb72b8c2bbd447d66a7f742e
azizulislam74 <azizulislam74@gmail.com>
2023-11-28 12:06:39 +0600

    footer add

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import MainLayout from "./Component/MainLayout/MainLayout";
 import Banner from "./Component/Pages/Banner/Banner/Banner";
+import Footer from "./Component/Footer/Footer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <MainLayout>
         <Banner></Banner>
         <Outlet />
+        <Footer></Footer>
       </MainLayout>
     </>
   );
