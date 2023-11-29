cee6b51b68abff66b5b72f83657f55c6cd5bd2e4
naim0018 <rakibhasan1101@gmail.com>
2023-11-29 19:40:23 +0600

    add cards

--- src/hooks/useData.jsx
+++ src/hooks/useData.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
-
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const useData = () => {
-    const [products,setProducts]=useState();
-    useEffect(()=>{
-        fetch('Product.json') 
-        .then(res => res.json())
-        .then(data => setProducts(data))
-    },[])
-    return products;
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("Product.json")
+      .then(res => res.json())
+      .then(data => setProducts(data))
+      .catch(error=>console.log(error))
+  },[]);
+  return products;
 };
 
 export default useData;
