20b2fa8a224cd995570f9fee90c6da6d9e553515
naim0018 <rakibhasan1101@gmail.com>
2023-12-01 21:35:39 +0600

    Update Card

--- src/Component/Pages/Products/Products.jsx
+++ src/Component/Pages/Products/Products.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import useData from '../../../hooks/useData';
+import Cards from '../Common/Cards/Cards';
 
 const Products = () => {
+    const products = useData();
+    console.log(products)
     return (
-        <div>
-            Inside Products
+        <div className='min-h-screen'>
+            <h1 className='text-5xl font-light text-center my-10'>Our Products</h1>
+            <div className="w-5/6 mx-auto grid lg:grid-cols-4 md:grid-cols-2 gap-5 justify-items-center my-10">
+                {
+                    products.map(data => <Cards key ={data.id} data={data}/>)
+                }
+            </div>
         </div>
     );
 };
