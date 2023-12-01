29ea7cc5ed2de5741cc67c03ae646ec07b429374
naim0018 <rakibhasan1101@gmail.com>
2023-12-01 21:35:39 +0600

    Update Card

--- src/Component/Pages/Common/Cards/Cards.jsx
+++ src/Component/Pages/Common/Cards/Cards.jsx
@@ -18,12 +18,12 @@ const Cards = ({ data }) => {
     <div className="border border-black rounded-lg">
       {/* <div className="w-full  border outline outline-1 border-b-4 border-r-4 border-transparent hover:border-b-4 hover:border-r-4 hover:border-b-black hover:border-r-black rounded-lg overflow-hidden"
       > */}
-      <div className="w-full border-b-4 border-r-4 border-transparent  hover:border-b-4 hover:border-r-4 hover:border-b-black hover:border-r-black rounded-lg overflow-hidden transition-all  ease-in-out"
+      <div className="w-full border-b-4 border-r-4 border-transparent group hover:border-b-4 hover:border-r-4 hover:border-b-black hover:border-r-black rounded-lg overflow-hidden transition-all  ease-in-out"
       >
         {/* image  */}
         <div className="grid relative">
           <img
-            className="place-self-center border drop-shadow-lg bg-transparent mt-4 hover:scale-105 transition-all ease-in-out"
+            className="place-self-center  bg-transparent mt-4 group-hover:scale-110 transition-all ease-in-out"
             src={image}
             alt=""
           />
