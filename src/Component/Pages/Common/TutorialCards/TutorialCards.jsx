adadeb403e0b65af171929c052bceaa8b6013a86
naim0018 <rakibhasan1101@gmail.com>
2023-12-01 14:06:10 +0600

    add Tools Card

--- src/Component/Pages/Common/TutorialCards/TutorialCards.jsx
+++ src/Component/Pages/Common/TutorialCards/TutorialCards.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React from "react";
 
-const TutorialCards = ({tutorial}) => {
-    const {img,title}=tutorial;
-    console.log(img);
-    return (
-        <div>
-           <p><img src={img} alt="" /></p> 
-          <p>{title}</p>
-        </div>
-    );
+const TutorialCards = ({ tutorial }) => {
+  const { img, title , number, description,  } = tutorial;
+  console.log(img);
+  return (
+    <div className="border flex  p-8 ">
+      
+        <img className="w-4/12 " src={img} alt="" />
+      
+      <div className="">
+        <p>{title}</p>
+
+      </div>
+    </div>
+  );
 };
 
 export default TutorialCards;
