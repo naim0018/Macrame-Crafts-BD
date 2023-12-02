f1b1179965e20a4f452f6258fcf9dad06cb46457
naim0018 <rakibhasan1101@gmail.com>
2023-12-02 20:30:20 +0600

    update

--- src/Component/Pages/Common/Details/CardDetails.jsx
+++ src/Component/Pages/Common/Details/CardDetails.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import useData from '../../../../hooks/useData';
+import { useParams } from 'react-router-dom';
 
 const CardDetails = () => {
+    const product = useData();
+    const id = useParams();
+    const productId = product.find(item => item.id === parseInt(id))
+
+    console.log(productId , id)
     return (
         <div>
-            
+            details
         </div>
     );
 };
