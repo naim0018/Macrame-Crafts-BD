8b073f679dab437af1a5396e82316a3181bb6f5f
naim0018 <rakibhasan1101@gmail.com>
2023-12-04 22:01:35 +0600

    update card details

--- src/Component/Pages/Common/Cards/Cards.jsx
+++ src/Component/Pages/Common/Cards/Cards.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Cards = ({ data }) => {
+  const [discount, setDiscount] = useState(false);
   const {
     id,
     title,
@@ -15,12 +16,15 @@ const Cards = ({ data }) => {
     meterial,
   } = data;
 
+  // if (discountPrice){
+  //   setDiscount(true)
+  // } 
+
   return (
     <div className="border border-black rounded-lg">
       {/* <div className="w-full  border outline outline-1 border-b-4 border-r-4 border-transparent hover:border-b-4 hover:border-r-4 hover:border-b-black hover:border-r-black rounded-lg overflow-hidden"
       > */}
-      <div className="w-full border-b-4 border-r-4 border-transparent group hover:border-b-4 hover:border-r-4 hover:border-b-black hover:border-r-black rounded-lg overflow-hidden transition-all  ease-in-out flex flex-col h-full"
-      >
+      <div className="w-full border-b-4 border-r-4 border-transparent group hover:border-b-4 hover:border-r-4 hover:border-b-black hover:border-r-black rounded-lg overflow-hidden transition-all  ease-in-out flex flex-col h-full">
         {/* image  */}
         <div className="grid relative">
           <img
@@ -32,22 +36,28 @@ const Cards = ({ data }) => {
         {/* title  */}
         <div className="p-5 flex-grow ">
           <h2 className="my-5 text-lg font-medium">{title}</h2>
-          
+
           <p className="my-5 text-sm text-gray-400">{description}</p>
-        
         </div>
         <div className="px-5">
           <hr className="border border-black" />
         </div>
         {/* Price  */}
         <div className="p-5 flex items-center justify-center gap-5">
-            <div className=" flex items-center justify-center gap-2">
-          <del className="text-sm">${price}</del>
+
+          {
+            discountPrice ?
+          <div className=" flex items-center justify-center gap-2">
+            <del className="text-sm">${price}</del>
             <p className="text-lg font-medium">${discountPrice}</p>
-            </div>
+          </div>
+          :
+          <p className="text-sm">${price}</p>
+
+          }
           <div className="">
             <Link to={`/cardDetails/${id}`}>
-            <button className="btn">Details</button>
+              <button className="btn">Details</button>
             </Link>
           </div>
         </div>
--- src/Component/Pages/Common/Details/CardDetails.jsx
+++ src/Component/Pages/Common/Details/CardDetails.jsx
@@ -1,52 +1,89 @@
-import React, { useState } from 'react';
-import useData from '../../../../hooks/useData';
-import { useParams } from 'react-router-dom';
+import React, { useState } from "react";
+import useData from "../../../../hooks/useData";
+import { useParams } from "react-router-dom";
+import { TbCurrencyTaka } from "react-icons/tb";
 
 const CardDetails = () => {
-    const [discount, setDiscount] = useState(false);
-    const product = useData();
-    const { id } = useParams();
-    // console.log(product)
-    const productId = product?.find(item => item.id == id)
-    const { title, image, height, width, description, price, discountPrice, material } = productId || {}
-    if (discountPrice) setDiscount(true)
+  const [discount, setDiscount] = useState(false);
+  const product = useData();
+  const { id } = useParams();
+  // console.log(product)
+  const productId = product?.find((item) => item.id == id);
+  const {
+    title,
+    image,
+    height,
+    width,
+    description,
+    price,
+    discountPrice,
+    material,
+  } = productId || {};
+  //   if (discountPrice) {
+  //     setDiscount(true);
+  //   }
 
+  return (
+    <div className="">
+      {productId && (
+        <div className="min-h-screen grid  p-8 md:p-- md:grid-cols-2 my-10 justify-center">
+          <div className="grid">
+            <img
+              className="w-4/5 place-self-center"
+              src={image}
+              alt={title}
+              title={title}
+            />
+          </div>
+          <div className="my-20  sticky top-10 h-fit">
+            <div className="space-y-5">
+              <h2 className="text-4xl font-medium">{title}</h2>
+              <p className="w-2/3 text-lg font-normal text-gray-500">
+                {description}
+              </p>
 
-    return (
-        <div className=''>
-            {productId &&
-                <div className='min-h-screen grid  p-8 md:p-- md:grid-cols-2 my-10 justify-center'>
-                    <div className="grid">
-                        <img className='w-4/5 place-self-center' src={image} alt={title} title={title} />
-                    </div>
-                    <div className="my-20  sticky top-10 h-fit">
-                        <div className="space-y-5">
-                            <h2 className='text-4xl font-medium'>{title}</h2>
-                            <p className='w-2/3 text-lg font-normal text-gray-500'>{description}</p>
+              <p>
+                <span className="text-lg font-semibold">Height : </span>{" "}
+                <span className="text-xl font-normal text-gray-500">
+                  {" "}
+                  {height}{" "}
+                </span>{" "}
+                <span className="h-20 border-l-[1px] border-black mx-5"></span>{" "}
+                <span className="text-lg font-semibold"> Width : </span>{" "}
+                <span className="text-xl font-normal text-gray-500">
+                  {width}
+                </span>
+              </p>
+              <p>
+                <span className="text-lg font-semibold">Material </span>:{" "}
+                <span className="text-lg font-normal text-gray-500 ">
+                  {material}
+                </span>
+              </p>
+            </div>
 
-                            <p><span className='text-lg font-semibold'>Height : </span> <span className='text-xl font-normal text-gray-500'> {height} </span> <span className="h-20 border-l-[1px] border-black mx-5"></span> <span className='text-lg font-semibold'> Width : </span> <span className='text-xl font-normal text-gray-500'>{width}</span></p>
-                            <p><span className='text-lg font-semibold'>Material </span>: <span className='text-lg font-normal text-gray-500 '>{material}</span></p>
-
-                        </div>
-
-                        <div className="mt-14">
-                            {
-                                discount
-                                    ?
-                                    <>
-                                        <del>{price}</del>
-                                        <button className='btn btn-neutral btn-lg btn-wide '>Price : {discountPrice}</button>
-                                    </>
-                                    :
-                                    <button className='btn btn-neutral btn-lg btn-wide '>Price : {discountPrice}</button>
-                            }
-
-                        </div>
-                    </div>
+            <div className="mt-14">
+              {discountPrice ? (
+                <div className="">
+                  <div className="flex gap-4 items-center w-fit">
+                    <p className="text-4xl font-medium flex items-center text-emerald-400"><TbCurrencyTaka />{discountPrice}</p>
+                    <del className="flex items-center text-red-400"> <TbCurrencyTaka />{price}</del>
+                  </div>
                 </div>
-            }
+              ) : (
+                <p className="text-4xl font-medium flex items-center text-emerald-400">
+                  <TbCurrencyTaka />{price}
+                </p>
+              )}
+              <div className="">
+                <button className="btn btn-lg bg-yellow-300 mt-8">Add to Cart</button>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default CardDetails;
