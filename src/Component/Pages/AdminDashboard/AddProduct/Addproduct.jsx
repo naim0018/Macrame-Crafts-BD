314cdcad214dc9d9d4e510302854165ae0bd3f01
azizulislam74 <azizulislam74@gmail.com>
2023-12-09 18:14:27 +0600

    add product input add done

--- src/Component/Pages/AdminDashboard/AddProduct/Addproduct.jsx
+++ src/Component/Pages/AdminDashboard/AddProduct/Addproduct.jsx
@@ -1,12 +1,65 @@
-
-import React from 'react';
+import React from "react";
 
 const Addproduct = () => {
-    return (
-        <div>
-            
+  return (
+    <>
+      <div className="hero min-h-screen border-4 pb-24 mb-20">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Product Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Product Name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Catagoary</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Catagoary"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Description</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Catagoary"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Price</span>
+                </label>
+                <input
+                  type="number"
+                  placeholder="Number"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn bg-yellow-400">Submit</button>
+              </div>
+            </form>
+          </div>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Addproduct;
--- src/Component/Pages/Tools/Tools.jsx
+++ src/Component/Pages/Tools/Tools.jsx
@@ -1,5 +1,6 @@
 
 
+import { Helmet } from "react-helmet-async";
 import useToolsData from "../../../hooks/useToolsData";
 import ToolCard from "../Common/ToolCard/ToolCard";
 const Tools = () => {
@@ -7,6 +8,9 @@ const Tools = () => {
     return (
         <div>
              <div className="container mx-auto my-10 place-content-center space-y-5">
+              <Helmet>
+                <title>Macrame Carft BD | Tools</title>
+              </Helmet>
       {
         tools.map(tool => <ToolCard key={tool.id} tool={tool}/>)
       }
