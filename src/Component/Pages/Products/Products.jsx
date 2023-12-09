89befa6b636b0b20f3232eaf0810617e79759ee7
azizulislam74 <azizulislam74@gmail.com>
2023-12-09 13:01:30 +0600

    title bar add

--- src/Component/Pages/Products/Products.jsx
+++ src/Component/Pages/Products/Products.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import useData from '../../../hooks/useData';
 import Cards from '../Common/Cards/Cards';
+import { Helmet } from 'react-helmet-async';
 
 const Products = () => {
     const products = useData();
     console.log(products)
     return (
         <div className='min-h-screen'>
+            <Helmet>
+                <title>Macrame Crafts BD | Our Product</title>
+            </Helmet>
             <h1 className='text-5xl font-light text-center my-10'>Our Products</h1>
             <div className="w-5/6 mx-auto grid lg:grid-cols-4 md:grid-cols-2 gap-5 justify-items-center my-10">
                 {
