2bba46b67e758a3f117290e36b45220559028ac7
azizulislam74 <azizulislam74@gmail.com>
2023-12-09 18:14:27 +0600

    add product input add done

--- src/Component/Pages/HomePage/Home/Home.jsx
+++ src/Component/Pages/HomePage/Home/Home.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import Featured from '../Featured/Featured';
 import HotDeals from '../HotDeals/HotDeals';
 import Banner from '../Banner/Banner/Banner';
+import { Helmet } from 'react-helmet-async';
 
 
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>
+                    Macrame Carft BD | Home
+                </title>
+            </Helmet>
             <Banner/> 
             <Featured/>
             <HotDeals/>
