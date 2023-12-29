982b908a9f7e8063fccd2449dac26237282fae43
azizulislam74 <azizulislam74@gmail.com>
2023-12-29 20:00:29 +0600

    not

--- src/Component/Pages/HomePage/Home/Home.jsx
+++ src/Component/Pages/HomePage/Home/Home.jsx
@@ -1,18 +1,12 @@
-<<<<<<< HEAD
 import React from "react";
 import Featured from "../Featured/Featured";
 import HotDeals from "../HotDeals/HotDeals";
 import Banner from "../Banner/Banner/Banner";
 import { Helmet } from "react-helmet-async";
 import MessengerCustomerChat from "react-messenger-customer-chat";
-=======
 
-import Featured from '../Featured/Featured';
-import HotDeals from '../HotDeals/HotDeals';
-import Banner from '../Banner/Banner/Banner';
-import { Helmet } from 'react-helmet-async';
->>>>>>> 2d4245125cbc707f5a4b7e342b6ed921715a3c2c
-import NewArrival from '../NewArrival/NewArrival';
+
+
 
 
 const Home = () => {
@@ -33,19 +27,6 @@ const Home = () => {
       <HotDeals />
     </div>
   );
-    return (
-        <div>
-            <Helmet>
-                <title>
-                    Macrame Carft BD | Home
-                </title>
-            </Helmet>
-            <Banner/> 
-            <Featured/>
-            <HotDeals/>
-            <NewArrival/>
-        </div>
-    );
 };
 
 export default Home;
