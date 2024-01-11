f2b353bf372818ca68a3fe39763c93810127c911
Zakaria Islam Robin <zakaria.pau49@gmail.com>
2024-01-12 02:38:08 +0600

    update product filter by category

--- src/Component/Pages/Products/Products.jsx
+++ src/Component/Pages/Products/Products.jsx
@@ -4,11 +4,29 @@ import Cards from "../Common/Cards/Cards";
 import { Helmet } from "react-helmet-async";
 import Loading from "../Loading/Loading";
 import Loadingui from "../Loading/Loadingui/Loadingui";
+import { Dropdown } from "primereact/dropdown";
 
 
 const Products = () => {
-  const result = useData();
-  const {data: products,isLoading}=result
+  const [products, setProducts] = useState()
+  const [category, setCategory] = useState()
+  const { data, isLoading, isFetching } = useData()
+  useEffect(() => {
+    if (data) {
+      setProducts(data);
+    }
+
+    if (category !== "") {
+      console.log(category);
+      const categoryData = data?.filter(item => item.category == category)
+      setProducts(categoryData)
+    }
+
+  }, [data, category]);
+
+  console.log(products?.length);
+  console.log(category);
+
 
   return (
     <div className="min-h-screen">
@@ -17,16 +35,25 @@ const Products = () => {
       </Helmet>
       <h1 className="text-5xl font-light text-center my-10">Our Products</h1>
 
-        {
-        isLoading ? <Loadingui/> :
-      <div className="w-5/6 mx-auto grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-5 justify-items-center my-10 ">
-        {
-          products.map((data) => (
-            <Cards key={data._id} data={data} />
-            ))
-        }
-      </div>
-        }
+
+      <select className="select select-bordered w-full max-w-xs" value={category} onChange={e => setCategory(e.target.value)}>
+        <option value="" >Select Category</option>
+        <option value="1 layer">1 layer</option>
+        <option value="2 layer">2 layer</option>
+        <option value="3 layer">3 layer</option>
+        <option value="Wall hanging">Wall Hanging</option>
+      </select>
+      {
+        isLoading ? <Loadingui /> :
+
+          <div className="w-5/6 mx-auto grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-5 justify-items-center my-10 ">
+            {
+              products?.map((data) => (
+                <Cards key={data._id} data={data} />
+              ))
+            }
+          </div>
+      }
     </div>
   );
 };
--- src/Component/Pages/Tools/Tools.jsx
+++ src/Component/Pages/Tools/Tools.jsx
@@ -6,22 +6,22 @@ import ToolCard from "../Common/ToolCard/ToolCard";
 import Loading from "../Loading/Loading";
 import Loadingui from "../Loading/Loadingui/Loadingui";
 const Tools = () => {
-  const tools =useToolsData()
-  const {data,isLoading}=tools
-  
-    return (
-        <div className="min-h-screen">
-             <div className="container mx-auto my-10 place-content-center space-y-5">
-              <Helmet>
-                <title>Macrame Carft BD | Tools</title>
-              </Helmet>
-      {
-        isLoading ? <><Loadingui/></> : 
-        data.map(tool => <ToolCard key={tool.id} tool={tool}/>)
-      }
+  const tools = useToolsData()
+  const { data, isLoading } = tools
+
+  return (
+    <div className="min-h-screen">
+      <div className="container mx-auto my-10 place-content-center space-y-5">
+        <Helmet>
+          <title>Macrame Carft BD | Tools</title>
+        </Helmet>
+        {
+          isLoading ? <><Loadingui /></> :
+            data.map(tool => <ToolCard key={tool._id} tool={tool} />)
+        }
       </div>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Tools;
