02a41138dbdadfed3a974ec55683d70da7fd14f3
naim0018 <rakibhasan1101@gmail.com>
2024-01-13 10:46:55 +0600

    update

--- src/hooks/useData.jsx
+++ src/hooks/useData.jsx
@@ -1,13 +1,12 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useQuery } from "react-query";
-
-// https://macrame-crafts-server.vercel.app/products
+// 
 const useData = (sortOrder,category) => {
   const result = useQuery({
     queryKey: ["products"],
     queryFn: async () => {
-      const { data } = await axios.get(`http://localhost:400/products?sort=${sortOrder}&category=${category}`)
+      const { data } = await axios.get(`https://macrame-crafts-server.vercel.app/products?sort=${sortOrder}&category=${category}`)
       console.log(data, sortOrder);
       return data;
     }
