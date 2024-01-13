a9398ab4b47912c5a4c676d6f560dbc2d46c0b7d
naim0018 <rakibhasan1101@gmail.com>
2024-01-13 10:46:55 +0600

    update

--- src/Component/Pages/Products/Products.jsx
+++ src/Component/Pages/Products/Products.jsx
@@ -6,16 +6,15 @@ import Loading from "../Loading/Loading";
 import Loadingui from "../Loading/Loadingui/Loadingui";
 import { Dropdown } from "primereact/dropdown";
 
-
 const Products = () => {
   // const [products, setProducts] = useState()
-  const [sortOrder, setSortOrder] = useState('asc');
+  const [sortOrder, setSortOrder] = useState("asc");
   const [sortedData, setSortedData] = useState([]);
-  const [category, setCategory] = useState("")
-  const { data, refetch, isLoading, isFetching } = useData(sortOrder,category)
+  const [category, setCategory] = useState("");
+  const { data, refetch, isLoading, isFetching } = useData(sortOrder, category);
   useEffect(() => {
     refetch();
-  }, [sortOrder,category])
+  }, [sortOrder, category]);
   console.log(sortOrder);
 
   return (
@@ -25,30 +24,37 @@ const Products = () => {
       </Helmet>
       <h1 className="text-5xl font-light text-center my-10">Our Products</h1>
 
-      <select className="select select-bordered w-full max-w-xs" value={sortOrder} onChange={e => setSortOrder(e.target.value)}>
+      <div className="w-5/6 mx-auto flex justify-end gap-5">
+        <select
+          className="select select-bordered w-full max-w-xs border-black font-bold"
+          value={sortOrder}
+          onChange={(e) => setSortOrder(e.target.value)}
+        >
+          <option value="asc">Price Low to High</option>
+          <option value="dsc">Price High to Low</option>
+        </select>
 
-        <option value="asc">Price Low to High</option>
-        <option value="dsc">Price High to Low</option>
-      </select>
-
-      <select className="select select-bordered w-full max-w-xs" value={category} onChange={e => setCategory(e.target.value)}>
-        <option value="" >Select Category</option>
-        <option value="1 layer">1 layer</option>
-        <option value="2 layer">2 layer</option>
-        <option value="3 layer">3 layer</option>
-        <option value="Wall hanging">Wall Hanging</option>
-      </select>
-      {
-        isLoading ? <Loadingui /> :
-
-          <div className="w-5/6 mx-auto grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-5 justify-items-center my-10 ">
-            {
-              data?.map((data) => (
-                <Cards key={data._id} data={data} />
-              ))
-            }
-          </div>
-      }
+        <select
+          className="select select-bordered w-full max-w-xs border-black font-bold"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="">Select Category</option>
+          <option value="1 layer">1 layer</option>
+          <option value="2 layer">2 layer</option>
+          <option value="3 layer">3 layer</option>
+          <option value="Wall hanging">Wall Hanging</option>
+        </select>
+      </div>
+      {isLoading ? (
+        <Loadingui />
+      ) : (
+        <div className="w-5/6 mx-auto grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-5 justify-items-center my-10 ">
+          {data?.map((data) => (
+            <Cards key={data._id} data={data} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
